5943ad99bef92fbf139a7039cc87e200c8f069f0
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-12-08 15:35:26 +0200

    connected the UI to the backend

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+from . import views as core_views
 
 urlpatterns  =  [
-    path('', views.home, name='Home'),
+    path('test/', core_views.home, name='Home'),
+    path('', core_views.about, name='About'),
+    path('contact/', core_views.contact, name='Contact'),
+    path('post/', core_views.post),
+    path('blog/', core_views.blog),
+
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,27 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Hello From Abo 6!</h1>')
+
+def contact(request):
+	context = {
+	'title' : 'Contact Us'
+	}
+	return render(request, 'contact.html', context)
+
+def about(request):
+	context = {
+	'title' : 'About Us'
+	}
+	return render(request, 'about.html', context)
+
+def blog(request):
+	context = {
+	'title' : 'Blog'
+	}
+	return render(request, 'index.html', context)
+
+def post(request):
+	context = {
+	'title' : 'Post'
+	}
+	return render(request, 'post.html', context)
