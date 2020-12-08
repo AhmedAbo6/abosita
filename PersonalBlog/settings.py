7dc29197401fe0aaf2d794e707abdbd01e3aa98d
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-12-08 14:33:07 +0200

    gitignore final

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'PersonalBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
