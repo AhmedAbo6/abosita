62a26234e3a0fb314c16670348118e587b8932c3
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-12-08 14:40:56 +0200

    changed the H1 in the home page

--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Hello!</h1>')
+    return HttpResponse('<h1>Hello From Abo 6!</h1>')
