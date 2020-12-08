a56b039cf0593dde8481858b30559c65d662688d
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-12-08 15:40:33 +0200

    made the posts app

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core.apps.CoreConfig',
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
